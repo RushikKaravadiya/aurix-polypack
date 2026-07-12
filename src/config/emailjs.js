491899f5e3fa53204ecb9137fea660651330f153
RushikKaravadiya <190540107102@darshan.ac.in>
2026-07-12 08:30:29 +0530

    email send on quote

--- src/config/emailjs.js
+++ src/config/emailjs.js
@@ -1,7 +1,7 @@
 const emailConfig = {
-  serviceId: import.meta.env.VITE_EMAILJS_SERVICE_ID || 'service_xyz',
-  templateId: import.meta.env.VITE_EMAILJS_TEMPLATE_ID || 'template_xyz',
-  publicKey: import.meta.env.VITE_EMAILJS_PUBLIC_KEY || 'public_key_xyz',
+  serviceId: import.meta.env.VITE_EMAILJS_SERVICE_ID || 'service_mu752y2',
+  templateId: import.meta.env.VITE_EMAILJS_TEMPLATE_ID || 'template_dknfibl',
+  publicKey: import.meta.env.VITE_EMAILJS_PUBLIC_KEY || 'ZgWhz2XHdvjFBNZwk',
 }
 
 export default emailConfig
